0405702cbf8aab02515807ed058fb9d59f5e7fb0
SYANDA MNTAMBO's GITHUB <112751864+syandagithub@users.noreply.github.com>
2023-07-17 19:57:47 +0200

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all single-digit numbers of base 10 starting from 0,
+ *              followed by a new line, using the putchar function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number = 0;
+
+	while (number <= 9)
+	{
+		putchar(number + '0');
+		number++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
