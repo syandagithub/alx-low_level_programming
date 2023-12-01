1ff1a95c351dfb9a6422e0f8a50a0170c00f9f4d
SYANDA MNTAMBO's GITHUB <112751864+syandagithub@users.noreply.github.com>
2023-12-01 14:43:15 +0200

    Add files via upload

---
+++ 0x1A-hash_tables/tests/3-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "../hash_tables.h"
+
+
+
+int main(void)
+{
+	hash_table_t *ht;
+
+	ht = hash_table_create(1024);
+	hash_table_set(ht, "betty", "holberton");
+	return (EXIT_SUCCESS);
+}
